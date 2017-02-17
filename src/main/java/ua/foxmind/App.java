f93106171597e456ee9ca0dad190f47cfee8f057
Viktor <viktor.relitskiy@gmail.com>
2017-02-17 13:11:24 +0200

    Changes on the comments

--- src/main/java/ua/foxmind/App.java
+++ src/main/java/ua/foxmind/App.java
@@ -2,35 +2,43 @@ package ua.foxmind;
 
 public class App {
 	public static void main(String[] args) {
-		String sourceWord = "a1bcd efg!h";
-		System.out.println(reverseWords(sourceWord));
+		String sourceString = "a1bcd efg!h";
+		System.out.println(splitString(sourceString));
 	}
 
-	static String reverseWords(String string) {
-		char[] sourceWordToChar = string.toCharArray();
+	static String splitString(String string) {
+		char[] sourceStringToChar = string.toCharArray();
 		int wordStartIndex = -1;
 
-		for (int i = 0; i < sourceWordToChar.length; i++) {
+		for (int i = 0; i < sourceStringToChar.length; i++) {
 			if (wordStartIndex < 0) {
 				wordStartIndex = i;
 			}
-			if (i + 1 == sourceWordToChar.length || sourceWordToChar[i + 1] == ' ') {
-				for (int firstCharInWord = wordStartIndex, lastCharInWord = i; firstCharInWord < lastCharInWord;) {
-					if (!Character.isAlphabetic(sourceWordToChar[firstCharInWord])) {
-						firstCharInWord++;
-					} else if (!Character.isAlphabetic(sourceWordToChar[lastCharInWord])) {
-						lastCharInWord--;
-					} else {
-						char tempChar = sourceWordToChar[firstCharInWord];
-						sourceWordToChar[firstCharInWord] = sourceWordToChar[lastCharInWord];
-						sourceWordToChar[lastCharInWord] = tempChar;
-						firstCharInWord++;
-						lastCharInWord--;
-					}
-				}
+			if (i + 1 == sourceStringToChar.length
+					|| sourceStringToChar[i + 1] == ' ') {
+				reversWord(sourceStringToChar, wordStartIndex, i);
 				wordStartIndex = -1;
 			}
 		}
-		return new String(sourceWordToChar);
+		return new String(sourceStringToChar);
+	}
+
+	private static void reversWord(char[] sourceStringToChar,
+			int wordStartIndex, int wordEndIndex) {
+		
+		while (wordStartIndex < wordEndIndex) {
+			if (!Character.isAlphabetic(sourceStringToChar[wordStartIndex])) {
+				wordStartIndex++;
+			} else if (!Character
+					.isAlphabetic(sourceStringToChar[wordEndIndex])) {
+				wordEndIndex--;
+			} else {
+				char tempChar = sourceStringToChar[wordStartIndex];
+				sourceStringToChar[wordStartIndex] = sourceStringToChar[wordEndIndex];
+				sourceStringToChar[wordEndIndex] = tempChar;
+				wordStartIndex++;
+				wordEndIndex--;
+			}
+		}
 	}
 }
--- src/test/java/ua/foxmind/AppTest.java
+++ src/test/java/ua/foxmind/AppTest.java
@@ -3,33 +3,47 @@ package ua.foxmind;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
-
-
 public class AppTest {
-    
+
 	@Test
-    public void testReversString() {
-        String reversString = App.reverseWords("a1bcd efg!h");
-        Assert.assertEquals("d1cba hgf!e", reversString);
-    }
-	
+	public void testReversString() {
+		String reversString = App.splitString("a1bcd efg!h");
+		Assert.assertEquals("d1cba hgf!e", reversString);
+	}
+
 	@Test
-    public void testReversStringWithDoubleSymbol() {
-        String reversString = App.reverseWords("a12bcd efg!?h");
-        Assert.assertEquals("d12cba hgf!?e", reversString);
-    }
-	
+	public void testReversStringWithDoubleSymbol() {
+		String reversString = App.splitString("a12bcd efg!?h");
+		Assert.assertEquals("d12cba hgf!?e", reversString);
+	}
+
 	@Test
-    public void testReversStringWithOnlySymbol() {
-        String reversString = App.reverseWords("12345 !?%@");
-        Assert.assertEquals("12345 !?%@", reversString);
-    }
-	
+	public void testReversStringWithOnlySymbol() {
+		String reversString = App.splitString("12345 !?%@");
+		Assert.assertEquals("12345 !?%@", reversString);
+	}
+
 	@Test
-    public void testReversThreeWords() {
-        String reversString = App.reverseWords("a1bcd efg!h 2rt4!");
-        Assert.assertEquals("d1cba hgf!e 2tr4!", reversString);
-    }
+	public void testReversThreeWords() {
+		String reversString = App.splitString("a1bcd efg!h 2rt4!");
+		Assert.assertEquals("d1cba hgf!e 2tr4!", reversString);
+	}
+
+	@Test
+	public void testReversSpaceStart() {
+		String reversString = App.splitString("  a1bcd efg!h");
+		Assert.assertEquals("  d1cba hgf!e", reversString);
+	}
+
+	@Test
+	public void testReversSpaceCenter() {
+		String reversString = App.splitString("a1bcd  efg!h");
+		Assert.assertEquals("d1cba  hgf!e", reversString);
+	}
+
+	@Test
+	public void testReversSpaceEnd() {
+		String reversString = App.splitString("a1bcd efg!h  ");
+		Assert.assertEquals("d1cba hgf!e  ", reversString);
+	}
 }
