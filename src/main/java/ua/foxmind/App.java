b2287f89535a0f1631959816bd76db86f07d8be0
Viktor <viktor.relitskiy@gmail.com>
2017-02-17 09:55:36 +0200

    Renamed variables, formatted code

--- src/main/java/ua/foxmind/App.java
+++ src/main/java/ua/foxmind/App.java
@@ -1,44 +1,36 @@
 package ua.foxmind;
 
+public class App {
+	public static void main(String[] args) {
+		String sourceWord = "a1bcd efg!h";
+		System.out.println(reverseWords(sourceWord));
+	}
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+	static String reverseWords(String string) {
+		char[] sourceWordToChar = string.toCharArray();
+		int wordStartIndex = -1;
 
-        String str = "a1bcd efg!h";
-        System.out.println(reverseWords(str));
-    }
-    
-    static String reverseWords(String string) {
-
-        char[] c = string.toCharArray();
-        int wordStartIndex = -1;
-
-        for (int i = 0; i < c.length; i++) {
-
-            if (wordStartIndex < 0) {
-                wordStartIndex = i;
-            }
-            if (i + 1 == c.length || c[i + 1] == ' ') {
-
-                for (int a = wordStartIndex, b = i; a < b; ) {
-
-                    if (!Character.isAlphabetic(c[a])) {
-                        a++;
-                    } else if (!Character.isAlphabetic(c[b])) {
-                        b--;
-                    } else {
-                        char tempChar = c[a];
-                        c[a] = c[b];
-                        c[b] = tempChar;
-                        a++;
-                        b--;
-                    }
-                }
-                wordStartIndex = -1;
-            }
-        }
-        return new String(c);
-    }
+		for (int i = 0; i < sourceWordToChar.length; i++) {
+			if (wordStartIndex < 0) {
+				wordStartIndex = i;
+			}
+			if (i + 1 == sourceWordToChar.length || sourceWordToChar[i + 1] == ' ') {
+				for (int firstCharInWord = wordStartIndex, lastCharInWord = i; firstCharInWord < lastCharInWord;) {
+					if (!Character.isAlphabetic(sourceWordToChar[firstCharInWord])) {
+						firstCharInWord++;
+					} else if (!Character.isAlphabetic(sourceWordToChar[lastCharInWord])) {
+						lastCharInWord--;
+					} else {
+						char tempChar = sourceWordToChar[firstCharInWord];
+						sourceWordToChar[firstCharInWord] = sourceWordToChar[lastCharInWord];
+						sourceWordToChar[lastCharInWord] = tempChar;
+						firstCharInWord++;
+						lastCharInWord--;
+					}
+				}
+				wordStartIndex = -1;
+			}
+		}
+		return new String(sourceWordToChar);
+	}
 }
--- src/test/java/ua/foxmind/AppTest.java
+++ src/test/java/ua/foxmind/AppTest.java
@@ -11,7 +11,25 @@ public class AppTest {
     
 	@Test
     public void testReversString() {
-        String str = App.reverseWords("a1bcd efg!h");
-        Assert.assertEquals("d1cba hgf!e", str);
+        String reversString = App.reverseWords("a1bcd efg!h");
+        Assert.assertEquals("d1cba hgf!e", reversString);
+    }
+	
+	@Test
+    public void testReversStringWithDoubleSymbol() {
+        String reversString = App.reverseWords("a12bcd efg!?h");
+        Assert.assertEquals("d12cba hgf!?e", reversString);
+    }
+	
+	@Test
+    public void testReversStringWithOnlySymbol() {
+        String reversString = App.reverseWords("12345 !?%@");
+        Assert.assertEquals("12345 !?%@", reversString);
+    }
+	
+	@Test
+    public void testReversThreeWords() {
+        String reversString = App.reverseWords("a1bcd efg!h 2rt4!");
+        Assert.assertEquals("d1cba hgf!e 2tr4!", reversString);
     }
 }
